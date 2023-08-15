4d6181e0d6bba1a05b67967a048a5fb7d9bfff6d
Charles Cheng <charles_cheng@microjet.com.tw>
2023-08-15 14:11:44 +0800

    just for fun

--- Demo_026/ViewController.swift
+++ Demo_026/ViewController.swift
@@ -9,11 +9,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImgView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fecthData()
+        
+        
+        let urlStr = "https://itunes.apple.com/search?term=swift&media=music"
+        if let url = URL(string: urlStr) {
+        URLSession.shared.dataTask(with: url) { data, response , error in
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        if let data = data {
+        do {
+        let searchResponse = try decoder.decode(SearchResponse.self, from: data)
+        print(searchResponse)
+        } catch {
+        print(error)
+        }
+        } else {
+        // show error
+        }
+        }.resume()
+        }
+
     }
 
 
+    func fecthData() {
+        let urlStr = "https://source.unsplash.com/random"
+        
+        
+        if let url = URL(string: urlStr) {
+            URLSession.shared.dataTask(with: url) { data, reponse, error in
+                
+                guard let data = data else { return }
+                let image = UIImage(data: data)
+                
+                DispatchQueue.main.async {
+                    self.myImgView.image = image
+                }
+                
+            }.resume()
+        }
+        
+        
+        
+       
+        
+        
+        
+        
+        
+    }
+    
+    
 }
 
